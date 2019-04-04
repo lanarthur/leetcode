49e4fb82e49011e02b37881f5b532bebd2ed91a9
lanarthur <lancelot.hga@gmail.com>
2019-04-04 08:49:50 +0800

    id=111, 二叉树最小深度

---
+++ 111.minimum-depth-of-binary-tree.go
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=111 lang=golang
+ *
+ * [111] Minimum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (34.83%)
+ * Total Accepted:    282.3K
+ * Total Submissions: 805.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its minimum depth.
+ *
+ * The minimum depth is the number of nodes along the shortest path from the
+ * root node down to the nearest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * return its minimum depth = 2.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left != nil && root.Right != nil {
+		left := minDepth(root.Left)
+		right := minDepth(root.Right)
+		if left < right {
+			return left + 1
+		}
+		return right + 1
+
+	} else if root.Left != nil {
+		return minDepth(root.Left) + 1
+	} else {
+		return minDepth(root.Right) + 1
+	}
+}
+
