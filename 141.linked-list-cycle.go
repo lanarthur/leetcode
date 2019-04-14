a0ddc387bfaf2c24e15dc6374a42c311f68fd85e
lanarthur <lancelot.hga@gmail.com>
2019-04-14 23:01:58 +0800

    id=141, 找到链表里的环

---
+++ 141.linked-list-cycle.go
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (35.68%)
+ * Total Accepted:    385.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Can you solve it using O(1) (i.e. constant) memory?
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slowP := head
+	fastP := head
+	for slowP != nil && fastP != nil && fastP.Next != nil {
+		slowP = slowP.Next
+		fastP = fastP.Next.Next
+		if slowP != nil && fastP != nil && slowP.Val == fastP.Val {
+			return true
+		}
+	}
+	return false
+}
+
